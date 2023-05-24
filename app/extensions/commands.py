1c69ee2141b57b2375767e89944813420107a6bc
roberto5g <robertofjv26@gmail.com>
2023-05-23 21:13:13 -0400

    adjusted commands to populate tables

--- app/extensions/commands.py
+++ app/extensions/commands.py
@@ -13,13 +13,22 @@ def drop_db():
     db.drop_all()
 
 
-def populate_db():
+def populate_router_table():
     """Populate db with sample data"""
     data = [
         Router(
             address="192.168.0.1"),
         Router(
-            address="192.168.0.2"),
+            address="192.168.0.2")
+    ]
+    db.session.bulk_save_objects(data)
+    db.session.commit()
+    return Router.query.all()
+
+
+def populate_user_table():
+    """Populate db with sample data"""
+    data = [
         User(
             login="roberto.santos",
             password="123123"
@@ -28,14 +37,13 @@ def populate_db():
             login="aldo.alex",
             password="123123"
         )
-
     ]
     db.session.bulk_save_objects(data)
     db.session.commit()
-    return Router.query.all()
+    return User.query.all()
 
 
 def init_app(app):
     # add multiple commands in a bulk
-    for command in [create_db, drop_db, populate_db]:
+    for command in [create_db, drop_db, populate_router_table, populate_user_table]:
         app.cli.add_command(app.cli.command()(command))
